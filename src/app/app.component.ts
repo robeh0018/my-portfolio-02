b5939a07face1eaaf29dea57e7a8c9bb45910e46
robeh0018 <robeh0018@gmail.com>
2024-05-25 14:47:25 -1100

    header section structure.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent, HtmlElementLabelComponent} from "./components";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+	imports: [RouterOutlet, HeaderComponent, HtmlElementLabelComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
